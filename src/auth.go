d4a803e69687d8e406a9c403235f24a275446022
zhengyi shi <shizy628@gmail.com>
2018-10-17 22:24:59 -0400

    implement get_student_courses in test.go

--- src/auth.go
+++ src/auth.go
@@ -13,11 +13,13 @@ var cookies map[string]string
 /* a helper function
 print all username and uuid stored in cookies
 */
-func helper(w http.ResponseWriter, r *http.Request) {
+/*
+func print_all_cookies(w http.ResponseWriter, r *http.Request) {
 	for username := range cookies {
 		fmt.Fprint(w, "username: ", username, " ", "uuid: ", cookies[username], "\n")
 	}
 }
+*/
 
 /*
 accept a string containing username
@@ -30,7 +32,7 @@ func set_cookie(w http.ResponseWriter, r *http.Request) {
 	username, _ := ioutil.ReadAll(r.Body)
 	expiration := time.Now().Add(time.Hour)
 	u1 := uuid.Must(uuid.NewV4()).String()
-	nameCookie := http.Cookie{Name: "username", Value: string(username)}
+	nameCookie := http.Cookie{Name: "username", Value: string(username), Expires: expiration}
 	cookie := http.Cookie{Name: string(username), Value: u1, Expires: expiration}
 	cookies[cookie.Name] = cookie.Value
 	http.SetCookie(w, &nameCookie)
@@ -45,24 +47,33 @@ nameCookie: the first one has Name: "username" and Value: string(username)
 Cookie: the second one has Name: string(username) and Value: uuid
 then check if the cookie we get has the valid uuid
 */
-func get_cookie(w http.ResponseWriter, r *http.Request) {
+func get_cookie(w http.ResponseWriter, r *http.Request) bool {
+	if cookies == nil {
+		cookies = make(map[string]string)
+	}
 	nameCookie, _ := r.Cookie("username")
 	if nameCookie == nil {
-		fmt.Fprint(w, "NM$L")
-		return
+		// fmt.Fprint(w, "NM$L")
+		return false
 	} else {
 		cookie, _ := r.Cookie(nameCookie.Value)
 		if cookie.Value != cookies[cookie.Name] {
-			w.WriteHeader(http.StatusUnauthorized)
-			fmt.Fprint(w, "INVALID COOKIE")
-			return
+			// w.WriteHeader(http.StatusUnauthorized)
+			// fmt.Fprint(w, "INVALID COOKIE")
+			return false
 		}
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, "OK")
+		// w.WriteHeader(http.StatusOK)
+		// fmt.Fprint(w, "OK")
+		return true
 	}
 }
 
-func get_students(w http.ResponseWriter, r *http.Request) {
+func get_student(w http.ResponseWriter, r *http.Request) {
+	if get_cookie(w, r) {
+		fmt.Fprint(w, "OK")
+	} else {
+		fmt.Fprint(w, "NM$L")
+	}
 	/* student1 := Student{"ziyi lu", "", false}
 	student2 := Student{"yanlin zhu", "", false}
 	student3 := Student{"jingfei zhou", "", false}
--- src/main.go
+++ src/main.go
@@ -7,11 +7,12 @@ import (
 
 func main() {
 	http.HandleFunc("/set", set_cookie)
-	http.HandleFunc("/get", get_cookie)
-	http.HandleFunc("/students", get_students)
+	//http.HandleFunc("/get", get_cookie)
+	http.HandleFunc("/students", get_student)
 	http.HandleFunc("/reg", register)
 	http.HandleFunc("/login", login)
-	http.HandleFunc("/check", helper)
+	http.HandleFunc("/courses", get_student_courses)
+	//http.HandleFunc("/check", print_all_cookies)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
