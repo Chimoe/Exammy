8af80965b4152f0d2f9a48c1e2cce01fcceb5c43
kaijianZ <zhongk@rpi.edu>
2018-10-29 16:24:53 -0400

    add getStudentTests function

--- src/main.go
+++ src/main.go
@@ -6,12 +6,13 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/set", set_cookie)
-	//http.HandleFunc("/get", get_cookie)
+	http.HandleFunc("/set", setCookie)
+	//http.HandleFunc("/get", getCookie)
 	http.HandleFunc("/students", get_student)
 	http.HandleFunc("/reg", register)
 	http.HandleFunc("/login", login)
-	http.HandleFunc("/courses", get_student_courses)
+	http.HandleFunc("/courses", getStudentCourses)
+	http.HandleFunc("/tests", getStudentTests)
 	//http.HandleFunc("/check", print_all_cookies)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
