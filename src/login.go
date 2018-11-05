f5d85a72bec3c9a64f21d55c4fcbc34027c538a3
zhengyi shi <shizy628@gmail.com>
2018-11-05 17:43:57 -0500

    give user student or instructor identity

--- src/login.go
+++ src/login.go
@@ -9,11 +9,12 @@ import (
 	"net/http"
 )
 
-type Student struct {
+type User struct {
 	RcsID     string
 	FirstName string
 	LastName  string
 	Password  string
+	Identity  bool //true: student   false: instructor
 }
 
 /*  a login API
@@ -22,19 +23,23 @@ return plain text indicating if the id and password are correct
 */
 func login(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	var a []byte
+
+	r.Body.Read(a)
+	fmt.Print(string(a))
 	if r.Body == nil {
 		http.Error(w, "Please send a request body", http.StatusBadRequest)
 		return
 	}
 
-	s := Student{}
-	err := json.NewDecoder(r.Body).Decode(&s)
+	u := User{}
+	err := json.NewDecoder(r.Body).Decode(&u)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	if s.RcsID == "" || s.Password == "" {
+	if u.RcsID == "" || u.Password == "" {
 		http.Error(w, "Please send a rcs_id and a password", http.StatusBadRequest)
 		return
 	}
@@ -46,7 +51,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	stmt, err := db.Prepare("SELECT password FROM student WHERE rcs_id = ?")
+	stmt, err := db.Prepare("SELECT password FROM user WHERE rcs_id = ? AND identity = ?")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -54,7 +59,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 	defer stmt.Close()
 
 	var hashedPassword []byte
-	stmt.QueryRow(s.RcsID).Scan(&hashedPassword)
+	stmt.QueryRow(u.RcsID, u.Identity).Scan(&hashedPassword)
 
 	if len(hashedPassword) == 0 {
 		w.WriteHeader(http.StatusBadRequest)
@@ -62,7 +67,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(s.Password))
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(u.Password))
 	if err != nil {
 		w.WriteHeader(http.StatusUnauthorized)
 		fmt.Fprint(w, "WRONG PASSWORD")
@@ -79,22 +84,22 @@ create a new student account and insert it into database
 return plain text indicating if the account are registered correctly
 */
 func register(w http.ResponseWriter, r *http.Request) {
-	s := Student{}
+	u := User{}
 	if r.Body == nil {
 		http.Error(w, "Please send a request body", http.StatusBadRequest)
 		return
 	}
-	err := json.NewDecoder(r.Body).Decode(&s)
+	err := json.NewDecoder(r.Body).Decode(&u)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	pwd, err := bcrypt.GenerateFromPassword([]byte(s.Password), bcrypt.DefaultCost)
+	pwd, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	s.Password = string(pwd)
+	u.Password = string(pwd)
 	db, err := sql.Open("mysql", dataSourceName)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -102,14 +107,15 @@ func register(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	stmt, err := db.Prepare("INSERT student SET rcs_id = ?, first_name = ?, last_name = ?, password = ?")
+	stmt, err := db.Prepare(`INSERT user SET rcs_id = ?, first_name = ?, last_name = ?, password = ?
+								   , identity = ?`)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer stmt.Close()
 
-	res, err := stmt.Exec(s.RcsID, s.FirstName, s.LastName, s.Password)
+	res, err := stmt.Exec(u.RcsID, u.FirstName, u.LastName, u.Password, u.Identity)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -123,7 +129,7 @@ func register(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "OK")
 	/*
 		w.Header().Set("Content-Type", "application/json")
-		js, err := json.Marshal(s)
+		js, err := json.Marshal(u)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
